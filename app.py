9b50d76d939c8e9b29379134e98367788be3a4d6
Breno de Barros <brenowbarros21@gmail.com>
2023-11-26 23:25:24 -0400

    Refactoring and implementing Flask RESTful

--- app.py
+++ app.py
@@ -1,77 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
+
+from purchase_orders.resources import PurchaseOrders
 
 app = Flask(__name__)
-
-purchase_orders = [
-    {
-        'id': 1,
-        'description': 'Pedido de Compra 1',
-        'items': [
-            {
-                'id': 1,
-                'description': 'Item do pedido 1',
-                'price': 49.90
-            }
-        ]
-    }
-
-]
-
-# GET purchase_orders
-# GET purchase_orders_by_id
-# POST purchase_orders
-# GET purchase_orders_items
-# POST purchase_orders_items
-
-@app.route('/')
-def home():
-    return "Hello World"
-
-@app.route('/purchase-orders')
-def get_purchase_orders():
-    return jsonify(purchase_orders)
-
-@app.route('/purchase-orders/<int:id>')
-def get_purchase_orders_by_id(id):
-    for po in purchase_orders:
-        if po['id'] == id:
-            return jsonify(po)
-        else:
-            return jsonify({'message': f'Pedido {id} não encontrado'})
-        
-@app.route('/purchase-orders/<int:id>/items')
-def get_purchase_orders_items(id):
-    for po in purchase_orders:
-        if po['id'] == id:
-            return jsonify(po['items'])
-        else:
-            return jsonify({'message': f'Pedido {id} não encontrado'})
-        
-@app.route('/purchase-orders', methods=['POST'])
-def create_purchase_order():
-    request_data = request.get_json()
-    purchase_order = {
-        'id': request_data['id'],
-        'description': request_data['description'],
-        'items': []
-    }
-
-    purchase_orders.append(purchase_order)
-    return jsonify(purchase_order)
-
-@app.route('/purchase-orders/<int:id>/items', methods=['POST'])
-def create_purchase_orders_items(id):
-    req_data = request.get_json()
-
-    for po in purchase_orders:
-        if po['id'] == id:
-            po['items'].append({
-                'id': req_data['id'],
-                'description': req_data['description'],
-                'price': req_data['price']
-            })
-            return jsonify(po)
-        else:
-            return jsonify({'message': f'Pedido {id} não encontrado'})
+api = Api(app)
+    
+api.add_resource(PurchaseOrders, '/purchase-orders')
 
 app.run(port=5000)
