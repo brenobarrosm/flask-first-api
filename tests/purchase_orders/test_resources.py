8a710ca26aa5421ed6f51e71193ec09a4305f258
Breno de Barros <brenowbarros21@gmail.com>
2024-02-27 23:46:36 -0400

    Test POST PurchaseOrders

--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -1,5 +1,24 @@
+import json
+
 def test_get_purchase_orders(test_client):
     response = test_client.get('/purchase-orders')
 
     assert response.status_code == 200
     assert response.json[0]['id'] == 1
+
+def test_post_purchase_orders(test_client):
+    obj = {
+        'id': 2,
+        'description': 'Purchase Order ID 2'
+    }
+
+    response = test_client.post(
+        '/purchase-orders',
+        data=json.dumps(obj),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 200
+    assert response.json['id'] == obj['id']
+    assert response.json['description'] == obj['description']
+    assert response.json['items'] == []
