1bd1313d9c6a6d8d6a1b45aded7282d21176fbb1
Breno de Barros <brenowbarros21@gmail.com>
2024-03-05 10:47:58 -0400

    Config Test DB

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ from db import db
 from purchase_orders.resources import PurchaseOrders, PurchaseOrderById
 from purchase_orders_items.resources import PurchaseOrdersItems
 
-def create_app():
+def create_app(env):
     app = Flask(__name__)
     api = Api(app)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/flask-restful'
+    database = 'flask-restful'
+    if env == 'testing':
+        database = 'flask-restful-test'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:admin@localhost:5432/{database}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
@@ -18,7 +22,7 @@ def create_app():
     api.add_resource(PurchaseOrderById, '/purchase-orders/<int:id>')
     api.add_resource(PurchaseOrdersItems, '/purchase-orders/<int:id>/items')
 
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
 
     return app
